6d9bcd1d2e13a444af74d358383942717419ec06
Michael <lygerzero0zero@gmail.com>
2018-03-02 19:18:53 +0900

    Update dependencies and clean up repo.

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 from setuptools import setup
 
+dependencies = [
+    'flask',
+    'flask-mail',
+    'flask-sqlalchemy',
+    'flask-sessionstore',
+    'flask-security',
+    'flask-socketio',
+    'flask-uploads',
+    'flask-wtf',
+    'bcrypt',
+    'dynet',
+    'eventlet',
+    'mysqlclient',
+    'redis'
+]
+
 setup(
     name='onitools',
     packages=['onitools'],
     include_package_data=True,
-    install_requires=['flask', 'flask-sqlalchemy', 'flask-session',
-                      'flask-security', 'flask-socketio', 'bcrypt', 'dynet']
+    install_requires=dependencies
 )
